ecedb5ab683ef4e9ba242d770678456fc1762962
lukatman <turmanidze26@icloud.com>
2026-04-05 00:30:26 +0100

    add: weight init function, seed initialization

--- train_gpt2.py
+++ train_gpt2.py
@@ -15,6 +15,7 @@ class CausalSelfAttention(nn.Module):
         self.c_attn = nn.Linear(config.n_embd, 3 * config.n_embd) 
         # output projection
         self.c_proj = nn.Linear(config.n_embd, config.n_embd)
+        self.c_proj.NANOGPT_SCALE_INIT = 1
         # regularization
         self.n_head = config.n_head
         self.n_embd = config.n_embd
@@ -55,6 +56,7 @@ class MLP(nn.Module):
         self.c_fc = nn.Linear(config.n_embd, 4 * config.n_embd)
         self.gelu = nn.GELU(approximate='tanh') # activation function
         self.c_proj = nn.Linear(4 * config.n_embd, config.n_embd)
+        self.c_proj.NANOGPT_SCALE_INIT = 1
     
     def forward(self, x):
         x = self.c_fc(x)
@@ -113,6 +115,19 @@ class GPT(nn.Module):
 
         # weight sharing scheme
         self.transformer.wte.weight = self.lm_head.weight
+
+        self.apply(self._init_weights)
+    
+    def _init_weights(self, module):
+        if isinstance(module, nn.Linear):
+            std = 0.02
+            if hasattr(module, 'NANOGPT_SCALE_INIT'):
+                std *= (2 * self.config.n_layer) ** -0.5
+            torch.nn.init.normal_(module.weight, mean=0.0, std=std)
+            if module.bias is not None:
+                torch.nn.init.zeros_(module.bias)
+        elif isinstance(module, nn.Embedding):
+            torch.nn.init.normal_(module.weight, mean=0.0, std=0.02)
 
     def forward(self, idx, targets=None):
         # idx is of shape (B, T)
@@ -238,6 +253,10 @@ else:
 device = "cpu"
 print(f"using device: {device}")
 
+torch.manual_seed(1337)
+if torch.mps.is_available():
+    torch.cuda.manual_seed(1337)
+
 train_loader = DataLoaderLite(B=4, T=32)
 
 # get logits
@@ -266,8 +285,6 @@ num_return_sequences = 5
 max_length = 30
 
 # generate! right now x is (B, T) where B = 5, T = 8
-torch.manual_seed(42)
-torch.mps.manual_seed(42)
 while x.size(1) < max_length:
     # forward the model to get the logits
     with torch.no_grad():
@@ -286,7 +303,7 @@ while x.size(1) < max_length:
         # append to the sequence
         x = torch.cat((x, xcol), dim=1)
 
-for i in range(num_return_sequences):
-    tokens = x[i, :max_length].tolist() # selects row i, up to max_length tokens
-    decoded = enc.decode(tokens)
-    print(">", decoded)
+# for i in range(num_return_sequences):
+#     tokens = x[i, :max_length].tolist() # selects row i, up to max_length tokens
+#     decoded = enc.decode(tokens)
+#     print(">", decoded)
